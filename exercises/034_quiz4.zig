ac72e99fab181ce0e8808182a6669c3838e6bf52
sayildiz <25555261+sayildiz@users.noreply.github.com>
2025-02-06 01:49:45 +0100

    ex 30 - 36

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -9,12 +9,18 @@ const std = @import("std");
 
 const NumError = error{IllegalNumber};
 
-pub fn main() void {
+pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
-    const my_num: u32 = getNumber();
+    const my_num: NumError!u32 = getNumber();
+    // this single line with  main() !void works too
+    // const my_num: u32 = getNumber() catch 0;
 
-    try stdout.print("my_num={}\n", .{my_num});
+    if (my_num) |value| {
+        try stdout.print("my_num={}\n", .{value});
+    } else |err| switch (err) {
+        NumError.IllegalNumber => unreachable,
+    }
 }
 
 // This function is obviously weird and non-functional. But you will not be changing it for this quiz.
